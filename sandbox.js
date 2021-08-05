c424a890c7a7cf32dfb8af7bbce39bcfab1e38a2
RayKilburg <rkilburg42@gmail.com>
2021-08-05 00:23:44 -0400

    #5 finish

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,25 @@
-var character = 'Peach';
-var age = 30;
-var isBlackBelt = false;
-character = 'mario';
-//age = 'yoshi'
-age = 40;
-//isBlackBelt = 'yes';
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(circ(7.5));
+// explict types
+var character;
+var age;
+var isLoggedIn;
+//age = 'luigi'
+age = 30;
+// isLoggedIn = 2;
+isLoggedIn = true;
+// arrays
+var ninjas = [];
+ninjas.push('shuan');
+// union types
+var mixed = [];
+mixed.push('hello');
+mixed.push(20);
+mixed.push(false);
+console.log(mixed);
+var uid;
+uid = '123';
+uid = 123;
+// objects
+var ninjaOne;
+ninjaOne = { name: 'yoshi', age: 30 };
+var ninjaTwo;
+ninjaTwo = { name: 'mario', age: 23, beltColor: 'black' };
